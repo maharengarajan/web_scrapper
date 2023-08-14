9b36bd80ac70ab8401df2c6a0d16e0dadc2d8ae9
RENGA RAJAN K <maharengarajan46@gmail.com>
2023-08-14 13:02:30 +0530

    app.py file completed

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as ureq
+import os
 
 app = Flask(__name__)
 
@@ -48,18 +49,39 @@ def index():
             #inside this name,rating,review heading and review body everything available
             commentboxes = prod_html.find_all('div', {'class': "_16PBlm"})
 
-            
+            # Create a directory to store the CSV files
+            directory = "search_csv_files"
+            if not os.path.exists(directory):
+                os.makedirs(directory)
 
+            #create file name and add it into directory
+            filename = os.path.join(directory, search_string + ".csv")
+            fw = open(filename, "w")
+            headers = "Product, Customer Name, Rating, Heading, Comment \n"
+            fw.write(headers)
 
+            reviews = []
+            for commentbox in commentboxes:
+                name = commentbox.div.div.find_all('p', {'class': '_2sc7ZR _2V5EHH'})[0].text
+                rating = commentbox.div.div.div.div.text
+                commentHead = commentbox.div.div.div.p.text
+                comtag = commentbox.div.div.find_all('div', {'class': ''})
+                customerComment = comtag[0].div.text
 
-        
-        
-        
-        
-        
-        
-        except:
-            pass
+                mydict = {"Product": search_string, "Name": name, "Rating": rating, "CommentHead": commentHead,
+                          "Comment": customerComment}
+                
+                reviews.append(mydict)
+            return render_template('results.html', reviews=reviews[0:(len(reviews)-1)])
+
+        except Exception as e:
+            print('The Exception message is: ',e)
+    
+    else:
+        return render_template('index.html')
+    
+if __name__ == "__main__":
+	app.run(debug=True)
 
 
 
